b34bf6cfe5c62bdb59cbd1ba99762929b19c0dd7
geuni <geuni0110@gmail.com>
2017-08-22 17:00:49 +0900

    init LeanWidget project

---
+++ LeanWidget/LeanWidget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeanWidget
+//
+//  Created by Yeongeun Song on 2017. 8. 22..
+//  Copyright © 2017년 Yeongeun Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
